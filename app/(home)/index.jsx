aa416251bdf4a8bbb75b83e9dbb061b477cf85f6
Isaac Njenga <ayzzoh20@gmail.com>
2025-08-12 16:30:57 +0300

    home page completed

--- app/(home)/index.jsx
+++ app/(home)/index.jsx
@@ -1,9 +1,11 @@
 import { salesDetails } from "@/assets/data/mockData";
+import { expenseData, salesData } from "@/assets/data/realData";
+import { getDashboardData } from "@/components/DashboardData";
 import SalesCarousel from "@/components/SalesCarousel";
 import { useAuthStore } from "@/providers/AuthStore";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Button, Card } from "@ui-kitten/components";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 const numbersDetails = [
@@ -32,16 +34,147 @@ const numbersDetails = [
 
 const HomeScreen = () => {
   const { logout } = useAuthStore();
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const [selectedPeriod, setSelectedPeriod] = useState("today");
+  const [day, setDay] = useState(null);
+  const [date, setDate] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [filteredSales, setFilteredSales] = useState([]);
+  const [filteredExpenses, setFilteredExpenses] = useState([]);
+  const currentDateTime = new Date();
+
+  const dashboardData = getDashboardData({ salesData, expenseData, day });
+
+  useEffect(() => {
+    setLoading(true);
+    try {
+      const filterByDateRange = (data, dateKey, days, specificDate = null) => {
+        let startDate, endDate;
+        if (specificDate) {
+          startDate = new Date(specificDate);
+          endDate = new Date(specificDate);
+        } else {
+          endDate = new Date();
+          endDate.setHours(23, 59, 59, 999);
+
+          startDate = new Date();
+          startDate.setDate(endDate.getDate() - days);
+        }
+
+        if (days === 1 && !specificDate) {
+          startDate.setDate(currentDateTime.getDate() - 1);
+          endDate.setDate(currentDateTime.getDate() - 1);
+        }
+
+        startDate.setHours(0, 0, 0, 0);
+        endDate.setHours(23, 59, 59, 999);
+
+        return data.filter((item) => {
+          const itemDate = new Date(item[dateKey]);
+          itemDate.setHours(0, 0, 0, 0);
+          return itemDate >= startDate && itemDate <= endDate;
+        });
+      };
+
+      let filteredSalesData = [];
+      let filteredExpensesData = [];
+
+      switch (selectedPeriod) {
+        case "today":
+          filteredSalesData = filterByDateRange(salesData, "datesold", 0);
+          filteredExpensesData = filterByDateRange(expenseData, "date", 0);
+          break;
+        case "yesterday":
+          filteredSalesData = filterByDateRange(salesData, "datesold", 1);
+          filteredExpensesData = filterByDateRange(expenseData, "date", 1);
+          break;
+        case "lastWeek":
+          filteredSalesData = filterByDateRange(salesData, "datesold", 7);
+          filteredExpensesData = filterByDateRange(expenseData, "date", 7);
+          break;
+        case "lastMonth":
+          filteredSalesData = filterByDateRange(salesData, "datesold", 30);
+          filteredExpensesData = filterByDateRange(expenseData, "date", 30);
+          break;
+        case "randomDay":
+          filteredSalesData = filterByDateRange(salesData, "datesold", 0, date);
+          filteredExpensesData = filterByDateRange(
+            expenseData,
+            "date",
+            0,
+            date
+          );
+          break;
+        default:
+          filteredSalesData = filterByDateRange(salesData, "datesold", 0);
+          filteredExpensesData = filterByDateRange(expenseData, "date", 0);
+      }
+
+      setFilteredSales(filteredSalesData);
+      setFilteredExpenses(filteredExpensesData);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }, [selectedPeriod, salesData, expenseData, date]);
+
+  const revenueMap = {
+    today: dashboardData.revenueToday,
+    yesterday: dashboardData.revenueYesterday,
+    lastWeek: dashboardData.revenueWeekly,
+    lastMonth: dashboardData.revenueMonthly,
+    randomDay: dashboardData.selectedDateRevenue,
+  };
+
+  const expensesMap = {
+    today: dashboardData.expenseToday,
+    yesterday: dashboardData.expenseYesterday,
+    lastWeek: dashboardData.expenseWeekly,
+    lastMonth: dashboardData.expenseMonthly,
+    randomDay: dashboardData.selectedDateExpense,
+  };
+
+  const profitMap = {
+    today: dashboardData.profitToday,
+    yesterday: dashboardData.profitYesterday,
+    lastWeek: dashboardData.profitWeekly,
+    lastMonth: dashboardData.profitMonthly,
+    randomDay: dashboardData.selectedDateProfit,
+  };
+
+  const commissionsMap = {
+    today: dashboardData.commissionsToday,
+    yesterday: dashboardData.commissionsYesterday,
+    lastWeek: dashboardData.commissionsWeekly,
+    lastMonth: dashboardData.commissionsMonthly,
+    randomDay: dashboardData.selectedDateCommissions,
+  };
+
+  const revenueDisplay = () =>
+    revenueMap[selectedRevenue] ?? dashboardData.revenueToday;
+  const expensesDisplay = () =>
+    expensesMap[selectedExpense] ?? dashboardData.expenseToday;
+  const commissionsDisplay = () =>
+    commissionsMap[selectedCommission] ?? dashboardData.commissionsToday;
+  const profitDisplay = () =>
+    profitMap[selectedProfit] ?? dashboardData.profitToday;
 
   return (
     <ScrollView style={styles.container}>
       {/* Header */}
       <View style={styles.headerContainer}>
-        <Button onPress={logout} style={styles.logoutBtn}>
-          Log out
-        </Button>
-        <Text style={styles.headerTextPrimary}>Sales Today</Text>
-        <Text style={styles.headerTextSecondary}>KES. 45,000</Text>
+        <View>
+          <Button onPress={logout} style={styles.logoutBtn}>
+            Log out
+          </Button>
+        </View>
+
+        <Card style={styles.header}>
+          <Text style={styles.headerTextPrimary}>Revenue</Text>
+          <Text style={styles.headerTextSecondary}>KES. 45,000</Text>
+        </Card>
       </View>
 
       {/* Cards - Overlapping */}
@@ -70,7 +203,63 @@ const HomeScreen = () => {
         </ScrollView>
       </View>
 
-      <SalesCarousel salesDetails={salesDetails} />
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={true}
+        contentContainerStyle={styles.cardScrollContent}
+      >
+        <Button
+          style={{ backgroundColor: "#008001", ...styles.buttonGroup }}
+          appearance="filled"
+          onPress={() => {
+            setSelectedPeriod("today");
+          }}
+        >
+          Today
+        </Button>
+        <Button
+          style={{ backgroundColor: "#4676c2", ...styles.buttonGroup }}
+          appearance="filled"
+          onPress={() => {
+            setSelectedPeriod("yesterday");
+          }}
+        >
+          Yesterday
+        </Button>
+        <Button
+          style={{ backgroundColor: "#f0b21a", ...styles.buttonGroup }}
+          appearance="filled"
+          onPress={() => {
+            setSelectedPeriod("lastWeek");
+          }}
+        >
+          Last 7 days
+        </Button>
+        <Button
+          style={{ backgroundColor: "#fb0102", ...styles.buttonGroup }}
+          appearance="filled"
+          onPress={() => {
+            setSelectedPeriod("lastMonth");
+          }}
+        >
+          Last 30 days
+        </Button>
+      </ScrollView>
+
+      <View>
+        <Text style={styles.salesHeader}>
+          Sales{" "}
+          {selectedPeriod === "lastWeek"
+            ? "for the last 7 days"
+            : selectedPeriod === "lastMonth"
+            ? "for the last 30 days"
+            : day
+            ? `for ${day}`
+            : selectedPeriod}
+          :
+        </Text>
+        <SalesCarousel salesDetails={salesDetails} />
+      </View>
     </ScrollView>
   );
 };
@@ -85,19 +274,30 @@ const styles = StyleSheet.create({
   },
   headerContainer: {
     backgroundColor: "#00152ae8",
-    paddingHorizontal: 20,
-    paddingVertical: 60,
+    paddingHorizontal: 10,
+    paddingBottom: 70,
+    paddingTop: 20,
+  },
+  header: {
+    backgroundColor: "rgba(254, 254, 254, 0)",
+    paddingVertical: 5,
+    borderRadius: 18,
+    width: 250,
+    marginHorizontal: 5,
+    borderColor: "#ffffff",
   },
   headerTextPrimary: {
     color: "whitesmoke",
     fontSize: 25,
     fontWeight: "400",
     lineHeight: 50,
+    color: "rgba(49, 255, 8, 1)",
   },
   headerTextSecondary: {
     color: "whitesmoke",
     fontSize: 40,
     fontWeight: "bold",
+    color: "rgba(49, 255, 8, 1)",
   },
 
   // Wrapper to create overlap
@@ -107,11 +307,12 @@ const styles = StyleSheet.create({
   cardScrollContent: {
     paddingHorizontal: 15,
     gap: 20,
+    marginVertical: 10,
   },
   card: {
     borderRadius: 18,
     padding: 5,
-    width: 200,
+    width: 250,
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 3 },
     shadowOpacity: 0.2,
@@ -121,5 +322,7 @@ const styles = StyleSheet.create({
   },
   cardTitle: { fontSize: 30, fontWeight: "600", lineHeight: 40 },
   cardText: { fontSize: 24, fontWeight: "bold" },
-});
+  salesHeader: { fontSize: 30, marginHorizontal: 20, lineHeight: 40 },
 
+  buttonGroup: { borderColor: "rgba(0,0,0,0)" },
+});
---
+++ components/DashboardData.js
@@ -0,0 +1,191 @@
+export function getDashboardData({ salesData, expenses, day }) {
+  //console.log(salesData);
+  let sales = salesData;
+  let expensesData = expenses;
+  // Helper function to filter sales and expenses by a specific date range
+  const filterByDateRange = (data, dateKey, days) => {
+    if (!Array.isArray(data)) return [];
+
+    const endDate = new Date();
+    endDate.setHours(23, 59, 59, 999);
+
+    const startDate = new Date();
+    startDate.setDate(endDate.getDate() - days);
+    startDate.setHours(0, 0, 0, 0);
+
+    const currentDateTime = new Date();
+    if (days === 1) {
+      startDate.setDate(currentDateTime.getDate() - 1);
+      endDate.setDate(currentDateTime.getDate() - 1);
+    }
+
+    return data.filter((item) => {
+      const dateValue = item?.[dateKey];
+      if (!dateValue) return false; // skip if missing or falsy
+
+      const itemDate = new Date(dateValue);
+      if (isNaN(itemDate)) return false; // skip invalid dates
+
+      itemDate.setHours(0, 0, 0, 0);
+      return itemDate >= startDate && itemDate <= endDate;
+    });
+  };
+
+  //filter random date
+  const filterSelectedDateDate = (data, dateKey, selectedDate) => {
+    if (!selectedDate || !Array.isArray(data)) return [];
+
+    const startDate = new Date(selectedDate);
+    startDate.setHours(0, 0, 0, 0);
+
+    const endDate = new Date(selectedDate);
+    endDate.setHours(23, 59, 59, 999);
+
+    return data.filter((item) => {
+      const dateValue = item?.[dateKey];
+      if (!dateValue) return false;
+
+      const itemDate = new Date(dateValue);
+      if (isNaN(itemDate)) return false;
+
+      itemDate.setHours(0, 0, 0, 0);
+      return itemDate >= startDate && itemDate <= endDate;
+    });
+  };
+
+  //  Filter today's data correctly
+  const filteredSales = filterByDateRange(sales, "datesold", 0);
+  const filteredExpenses = filterByDateRange(expensesData, "date", 0);
+
+  const revenueToday = filteredSales?.reduce(
+    (acc, sale) => acc + sale.total,
+    0
+  );
+  const commissionsToday = filteredSales?.reduce(
+    (acc, sale) => acc + sale.commission,
+    0
+  );
+
+  const expenseToday = filteredExpenses?.reduce(
+    (acc, expense) => acc + expense.cost,
+    0
+  );
+
+  const profitToday =
+    Number(revenueToday) - Number(expenseToday + commissionsToday);
+
+  // Yesterday's data
+  const filteredSalesYesterday = filterByDateRange(sales, "datesold", 1);
+  const filteredExpensesYesterday = filterByDateRange(expensesData, "date", 1);
+
+  const revenueYesterday = filteredSalesYesterday?.reduce(
+    (acc, sale) => acc + sale.total,
+    0
+  );
+  const commissionsYesterday = filteredSalesYesterday?.reduce(
+    (acc, sale) => acc + sale.commission,
+    0
+  );
+  const expenseYesterday = filteredExpensesYesterday?.reduce(
+    (acc, expense) => acc + expense.cost,
+    0
+  );
+
+  const profitYesterday =
+    Number(revenueYesterday) - Number(expenseYesterday + commissionsYesterday);
+
+  // Weekly data
+  const filteredSalesLastWeek = filterByDateRange(sales, "datesold", 7);
+  const filteredExpensesLastWeek = filterByDateRange(expensesData, "date", 7);
+
+  const revenueWeekly = filteredSalesLastWeek?.reduce(
+    (acc, sale) => acc + sale.total,
+    0
+  );
+  const commissionsWeekly = filteredSalesLastWeek?.reduce(
+    (acc, sale) => acc + sale.commission,
+    0
+  );
+  const expenseWeekly = filteredExpensesLastWeek?.reduce(
+    (acc, expense) => acc + expense.cost,
+    0
+  );
+
+  const profitWeekly =
+    Number(revenueWeekly) - Number(expenseWeekly + commissionsWeekly);
+
+  // Monthly data
+  const filteredSalesLastMonth = filterByDateRange(sales, "datesold", 31);
+  const filteredExpensesLastMonth = filterByDateRange(expensesData, "date", 31);
+
+  const revenueMonthly = filteredSalesLastMonth?.reduce(
+    (acc, sale) => acc + sale.total,
+    0
+  );
+  const commissionsMonthly = filteredSalesLastMonth?.reduce(
+    (acc, sale) => acc + sale.commission,
+    0
+  );
+
+  const expenseMonthly = filteredExpensesLastMonth?.reduce(
+    (acc, expense) => acc + expense.cost,
+    0
+  );
+
+  const profitMonthly =
+    Number(revenueMonthly) - Number(expenseMonthly + commissionsMonthly);
+
+  //filter selectedDate data
+  const filteredSelectedDateSales = filterSelectedDateDate(
+    sales,
+    "datesold",
+    day
+  );
+  const filteredSelectedDateExpenses = filterSelectedDateDate(
+    expensesData,
+    "date",
+    day
+  );
+
+  const selectedDateRevenue = filteredSelectedDateSales?.reduce(
+    (acc, sale) => acc + sale.total,
+    0
+  );
+
+  const selectedDateCommissions = filteredSelectedDateSales?.reduce(
+    (acc, sale) => acc + sale.commission,
+    0
+  );
+
+  const selectedDateExpense = filteredSelectedDateExpenses?.reduce(
+    (acc, expense) => acc + expense.cost,
+    0
+  );
+
+  const selectedDateProfit =
+    Number(selectedDateRevenue) -
+    Number(selectedDateExpense + selectedDateCommissions);
+
+  return {
+    revenueToday,
+    revenueYesterday,
+    revenueWeekly,
+    revenueMonthly,
+    selectedDateRevenue,
+    expenseToday,
+    expenseYesterday,
+    expenseWeekly,
+    expenseMonthly,
+    selectedDateExpense,
+    commissionsToday,
+    commissionsYesterday,
+    commissionsWeekly,
+    commissionsMonthly,
+    selectedDateCommissions,
+    profitToday,
+    profitYesterday,
+    profitWeekly,
+    profitMonthly,
+    selectedDateProfit,
+  };
+}
